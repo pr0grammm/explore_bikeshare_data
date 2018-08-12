383ce9d47250200062bf0aaa47fa5733edd258f1
Reshma <reshma.ramaprasad@gmail.com>
2018-08-12 19:11:26 +0530

    use sessions

--- bikeshare/__init__.py
+++ bikeshare/__init__.py
@@ -3,5 +3,5 @@ from flask import Flask
 
 #create Flask instance
 app = Flask(__name__)
-
+app.secret_key = 'topsecret'
 from bikeshare import views
--- bikeshare/views.py
+++ bikeshare/views.py
@@ -2,12 +2,8 @@
 from bikeshare import app
 from bikeshare import stats as st
 from bikeshare import rawdata as rd
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, session
 from bikeshare.icons import icond
-
-df_orig = None
-df = None
-current_city = None #city currently loaded in df
 
 #home page
 @app.route('/')
@@ -16,46 +12,50 @@ def index():
 
 # route to handle form submit
 @app.route('/postdata', methods = ['post'])
-def postdata():
-    global current_city, df_orig, df
-    data = request.form
+def postdata(start =0):
 
-    #get parameters
-    city=data['city']
-    month = data['month']
-    day = data['day']
+	if request.method == 'POST':
+		data = request.form
+		#get parameters
+		city=data['city']
+		month = data['month']
+		day = data['day']
+		session['city'] = city
+		session['month'] = month
+		session['day'] = day;
+		#get DataFrame
+		df_orig = st.load_city(city)
+		current_city=city
+		# get modified DataFrame
+		df, month_filter_set, day_filter_set = st.adjust_timeframe(df_orig, month, day)
 
-    #get DataFrame
-    if city != current_city:
-        df_orig = st.load_city(city)
-        current_city=city
+		if data['submit'] == 'Get Statistics':
+			time_stats = st.time_stats(df, month_filter_set, day_filter_set)
+			station_stats = st.station_stats(df)
+			trip_duration_stats = st.trip_duration_stats(df)
+			user_stats = st.user_stats(df)
 
-    # get modified DataFrame
-    df, month_filter_set, day_filter_set = st.adjust_timeframe(df_orig, month, day)
+			page ='stats.html'
+			return render_template(page, Time_stats=time_stats,\
+			Station_stats=station_stats, Trip_duration_stats=trip_duration_stats,\
+			User_stats=user_stats, Icond=icond, trim_blocks =True)
 
-    if data['submit'] == 'Get Statistics':
-        #calculate statistics
-        time_stats = st.time_stats(df, month_filter_set, day_filter_set)
-        station_stats = st.station_stats(df)
-        trip_duration_stats = st.trip_duration_stats(df)
-        user_stats = st.user_stats(df)
+		elif data['submit'] == 'View Raw Data':
+			table= rd.retdf(df,current_city,0)
+			page = 'viewraw.html'
+			return render_template(page,City=city, \
+			Table = table, trim_blocks=True)
 
-        #return page
-        page ='stats.html'
-        return render_template(page, Time_stats=time_stats,\
-        Station_stats=station_stats, Trip_duration_stats=trip_duration_stats,\
-        User_stats=user_stats, Icond=icond, trim_blocks =True)
-
-    elif data['submit'] == 'View Raw Data':
-        #get first 5 rows of DataFrame
-        table= rd.retdf(df,current_city,0)
-        page = 'viewraw.html'
-        return render_template(page,City=city, \
-        Table = table, trim_blocks=True)
-
+		
 
 @app.route('/gimmeHTML/<int:start>')
 def gimmeHTML(start):
-    """get HTML corresponding to 5 rows of DataFrame"""
-    dfhtml= rd.retdf(df, current_city, start)
-    return dfhtml
+	city=session.get('city',None)
+	month = session.get('month',None)
+	day = session.get('day',None)
+	df_orig = st.load_city(city)
+	current_city=city
+	# get modified DataFrame
+	df, month_filter_set, day_filter_set = st.adjust_timeframe(df_orig, month, day)
+	table= rd.retdf(df,city,start)
+	return table
